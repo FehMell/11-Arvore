19387f13aa68729ba1660155cbdeba3dc8c47891
Feh Mell <mell39654@gmail.com>
2024-05-14 10:46:16 -0300

    commit

--- ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
@@ -177,11 +177,21 @@ void exibirElementosArvore(NO* no)
 
 }
 
-void buscarElementoArvore(NO* no, int valor)
-{
-
-
+void buscarElementoArvore(NO* no, int valor) {         
+	if (no == NULL) {         cout << "Valor nao encontrado na arvore." << endl; 
+	return;     
+	}         
+	if (no->valor == valor) {         
+		cout << "Valor encontrado: " << no->valor << endl;         
+		return;     
+	}     
+	if (valor < no->valor) {
+		buscarElementoArvore(no->esq, valor);     
+	}          
+	else {         buscarElementoArvore(no->dir, valor);     
+	} 
 }
+		
 
 
 
